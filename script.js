7d7b4098f2b1aea77c1c90899c8246b181dc4bab
Muhammad Uzair <uzairyasin83@gmail.com>
2025-01-17 12:18:17 +0500

    first

---
+++ script.js
@@ -0,0 +1,45 @@
+let currentPlayer = "X";
+let arr = [null, null, null, null, null, null, null, null, null];
+
+function resetBoard() {
+    location.reload(); // Reloads the current page
+
+}
+function checkWinner() {
+    if (
+        (arr[0] !== null && arr[0] === arr[1] && arr[1] === arr[2]) ||
+        (arr[3] !== null && arr[3] === arr[4] && arr[4] === arr[5]) ||
+        (arr[6] !== null && arr[6] === arr[7] && arr[7] === arr[8]) ||
+        (arr[0] !== null && arr[0] === arr[3] && arr[3] === arr[6]) ||
+        (arr[1] !== null && arr[1] === arr[4] && arr[4] === arr[7]) ||
+        (arr[2] !== null && arr[2] === arr[5] && arr[5] === arr[8]) ||
+        (arr[0] !== null && arr[0] === arr[4] && arr[4] === arr[8]) ||
+        (arr[2] !== null && arr[2] === arr[4] && arr[4] === arr[6])
+    ) {
+        alert(`Player ${currentPlayer} wins!`)
+        resetBoard()
+        return;
+    }
+    if (!arr.some((e) => e === null)) {
+        alert("It's a tie!");
+        resetBoard();
+    }
+}
+
+
+
+function handleClick(el){
+
+    const id = Number(el.id)
+    if(arr[id]!== null){
+        return
+    }
+    arr[id] = currentPlayer;
+    el.innerText = currentPlayer  
+    checkWinner()
+    if (currentPlayer === "X"){
+        currentPlayer = "O";
+    }else {
+        currentPlayer = "X";
+    }
+}
